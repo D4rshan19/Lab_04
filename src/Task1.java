0ff1e258669fd3786433b11e1e824dcd48410e72
D4rshan19 <dhimal.621185@swl.k12.oh.us>
2025-09-08 08:55:54 -0400

    task1

---
+++ src/Task1.java
@@ -0,0 +1,9 @@
+public class Task1 {
+    public static void main(String[] args) {
+        int intOperandA = 12;
+        int intOperandB = 4;
+        int intSum = intOperandA + intOperandB;
+        int intDifference = intOperandA - intOperandB;
+        int intProduct = intOperandA * intOperandB
+    }
+}
