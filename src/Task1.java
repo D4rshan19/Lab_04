f03b9f9620a01cbbd25f1c1ac4007ae6d49f5417
D4rshan19 <dhimal.621185@swl.k12.oh.us>
2025-09-09 08:42:31 -0400

    task1

--- src/Task1.java
+++ src/Task1.java
@@ -3,7 +3,26 @@ public class Task1 {
         int intOperandA = 12;
         int intOperandB = 4;
         int intSum = intOperandA + intOperandB;
+        System.out.println("The sum using ints of " + intOperandA + " " + intOperandB + " is " + intSum);
         int intDifference = intOperandA - intOperandB;
-        int intProduct = intOperandA * intOperandB
+        System.out.println("The difference using ints of " + intOperandA + " " + intOperandB + " is " + intDifference);
+        int intProduct = intOperandA * intOperandB;
+        System.out.println("The product using ints of " + intOperandA + " " + intOperandB + " is " + intProduct);
+        int intQuotient = intOperandA / intOperandB;
+        System.out.println("The quotient using ints of " + intOperandA + " " + intOperandB + " is " + intQuotient);
+        int intModulo = intOperandA % intOperandB;
+        System.out.println("The modulus division using ints of " + intOperandA + " " + intOperandB + " is " + intModulo);
+        double doubleOperandA = 10.2;
+        double doubleOperandB = 5.6;
+        double doubleSum = doubleOperandA + doubleOperandB;
+        System.out.println("The sum using doubles of " + doubleOperandA + " " + doubleOperandB + " is " + doubleSum);
+        double doubleProduct = doubleOperandA * doubleOperandB;
+        System.out.println("The product using doubles of " + doubleOperandA + " " + doubleOperandB + " is " + doubleProduct);
+        double doubleDifference = doubleOperandA - doubleOperandB;
+        System.out.println("The difference using doubles of " + doubleOperandA + " " + doubleOperandB + " is " + doubleDifference);
+        double doubleQuotient = doubleOperandA / doubleOperandB;
+        System.out.println("The quotient using doubles of " + doubleOperandA + " " + doubleOperandB + " is " + doubleQuotient);
+        double doubleModulo = doubleOperandA % doubleOperandB;
+        System.out.println("The modulus division using doubles of " + doubleOperandA + " " + doubleOperandB + " is " + doubleModulo);
     }
 }
