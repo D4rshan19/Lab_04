d51ee31b278e0078a14655523f3eba1f78a51beb
D4rshan19 <dhimal.621185@swl.k12.oh.us>
2025-09-11 08:17:07 -0400

    task4

--- src/Task4.java
+++ src/Task4.java
@@ -2,8 +2,9 @@ public class Task4 {
     public static void main(String[] args) {
         double INTEREST_RATE = 0.17;
         double initialBalance = 5000;
-        double newBalance = (initialBalance * INTEREST_RATE) + initialBalance;
-        System.out.println("Your credit card balance after one month is $" + newBalance);
-        System.out.println("Your credit card balance after two months is $" + newBalance + initialBalance);
+        double oneBalance = (initialBalance * INTEREST_RATE) + initialBalance;
+        double twoBalance = (oneBalance * INTEREST_RATE) + oneBalance;
+        System.out.println("Your credit card balance after one month is $" + oneBalance);
+        System.out.println("Your credit card balance after two months is $" + twoBalance);
     }
 }
